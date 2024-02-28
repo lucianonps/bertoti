37e0a39302d0d6ee69c0e9fb23a3dbdbac5f5b73
lulu20000 <luciano.nascimentopamplona@gmail.com>
2024-02-28 19:34:21 -0300

    atualização

--- padões de projetos/strategy/src/App.java
+++ padões de projetos/strategy/src/App.java
@@ -1,11 +1,11 @@
 
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        cliente en = new cliente();
-        en.setEmprestimo(new permitido());
-        en.fazerEmprestimo();
-        
+public static void main(String[] args) throws Exception {
+    cliente en = new cliente();
+    en.setEmprestimo(new permitido());
+    en.fazerEmprestimo();
+    
 
-    }
+}
 }
